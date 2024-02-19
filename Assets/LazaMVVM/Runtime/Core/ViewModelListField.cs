67f323549762b84c0f7255c14d77c0989fa70b19
rsdal <rafael.securato@gmail.com>
2024-02-19 08:26:40 +0000

    Remove serializable item to avoid mistakes

--- Assets/LazaMVVM/Runtime/Core/ViewModelListField.cs
+++ Assets/LazaMVVM/Runtime/Core/ViewModelListField.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[Serializable]
 public class ViewModelListField<T> : IViewModelField, IList<T> where T : BaseViewModel
 {
     [SerializeField] 
