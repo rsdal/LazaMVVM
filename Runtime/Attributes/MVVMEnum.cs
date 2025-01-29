fbbb9fe1b9983c82a0d671e8739424902b283e0c
Laza <rafael.securato@gmail.com>
2025-01-29 14:27:18 +0000

    Add files via upload

---
+++ Runtime/Attributes/MVVMEnum.cs
@@ -0,0 +1,12 @@
+// Copyright Miniclip SA, 2025. All rights reserved.
+
+using System;
+
+namespace LazaMVVM.Runtime.Attributes
+{
+    [AttributeUsage(AttributeTargets.Enum, AllowMultiple = true)]
+    public class MVVMEnum : Attribute
+    {
+    
+    }
+}
