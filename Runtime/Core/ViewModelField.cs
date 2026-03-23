80296462dedd066e5bb2627af2e2336452771016
rsdal <rafael.securato@gmail.com>
2026-03-23 12:26:51 -0300

    Add new logic to notify view model if it changes inside of the inspector

--- Runtime/Core/ViewModelField.cs
+++ Runtime/Core/ViewModelField.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 namespace LazaMVVM.Runtime.Core
 {
     [Serializable]
-    public class ViewModelField<T> : IViewModelField
+    public class ViewModelField<T> : IViewModelField, ISerializationCallbackReceiver
     {
         [SerializeField]
         protected T propertyValue;
     
+        private T _lastInspectorValue;
+        
         public T Value
         {
             get => propertyValue;
@@ -30,5 +32,27 @@ namespace LazaMVVM.Runtime.Core
         {
             propertyValue = (T)value;
         }
+        
+        void ISerializationCallbackReceiver.OnBeforeSerialize() { }
+
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
+        {
+            if (EqualityComparer<T>.Default.Equals(propertyValue, _lastInspectorValue)) return;
+            
+            _lastInspectorValue = propertyValue;
+                
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.delayCall += NotifyFromInspector;
+#endif
+        }
+
+#if UNITY_EDITOR
+        private void NotifyFromInspector()
+        {
+            UnityEditor.EditorApplication.delayCall -= NotifyFromInspector;
+            OnValueChanged?.Invoke(propertyValue);
+        }
+#endif
+        
     }
 }
