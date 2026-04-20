e8ce33e8f2e9e32bea353b8bb33433b64e3d2852
rsdal <rafael.securato@gmail.com>
2026-04-20 10:57:38 -0300

    Added new select bind to change focus

---
+++ Runtime/Binding/SelectBind.cs
@@ -0,0 +1,31 @@
+﻿using LazaMVVM.Runtime.Attributes;
+using LazaMVVM.Runtime.Core;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[BindFilter(typeof(bool))]
+[RequireComponent(typeof(Button))]
+public class SelectBind : BaseFieldBind<bool>
+{
+    Button button;
+    
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
+    protected override void OnValueChanged(bool newValue)
+    {
+        if (button == null)
+        {
+            Debug.LogWarning("You tried to set the value before the text initialized");
+            return;
+        }
+
+        if (newValue)
+        {
+            EventSystem.current.SetSelectedGameObject(button.gameObject);
+        }
+    }
+}
